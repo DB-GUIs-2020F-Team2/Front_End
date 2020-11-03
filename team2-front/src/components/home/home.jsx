75024ae2f66f81f223de0105f38ae96d573c3311
veritas919 <olackner@smu.edu>
2020-11-03 09:25:37 -0600

    styling added to home page

--- team2-front/src/components/home/home.jsx
+++ team2-front/src/components/home/home.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import './home.css';
 import {Container, Row} from 'react-bootstrap';
+import './home.css';
 
 export class Home extends React.Component{ 
 
 render(){
-      return <>
-    <Container className = "justify-content-center">
-      <Row>
-      <h1> Project Tracker</h1>
+      return <div className = "jumbotron jumbotron-fluid ">
+    <Container className = "justify-content-center my-5 container-fluid mt-0 pt-0 bt-0">
+      <Row className = "justify-content-center">
+            <h1> Project Tracker</h1>
       </Row>
 
-      <Row>     
-    <p> Project tracking and coordination for the Defense industry. </p>
-    </Row>
+      <Row className = "justify-content-center">     
+            <p> Project tracking and coordination for the Defense industry. </p>
+      </Row>
 
-      <Row>
-    {/*  add this back when props functions are set up and other pages are made <button onClick={() => this.onLoginClick()}  */}
-    <button type="button" className = "btn btn-primary">
-          Login
-    </button>
-    </Row>
+      <Row className = "justify-content-center mt-4">
+            {/*  add this back when props functions are set up and other pages are made <button onClick={() => this.onLoginClick()}  */}
+            <button type="button" className = "btn btn-primary">
+            Login
+            </button>
+      </Row>
 
-      <Row>
-    <button type="button" className = "btn btn-primary">
+      <Row className = "justify-content-center mt-4">
+            <button type="button" className = "btn btn-primary">
           Register
-    </button>
-    </Row> 
+            </button>
+      </Row> 
       
     </Container>
-    </>;
+    </div>;
       
 }
 }
