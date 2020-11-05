5fa623c9e77a98ddcfa395cb1e227cced3131037
mj3wc <wcronk@smu.edu>
2020-11-04 19:39:40 -0600

    register page

--- team2-front/src/index.js
+++ team2-front/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Register from './register/register';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Register />
   </React.StrictMode>,
   document.getElementById('root')
 );
